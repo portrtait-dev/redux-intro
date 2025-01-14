6b32113b1e050329b3f9fd92c71c4e5c143b58fe
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-01-15 00:04:05 +0500

    🛠️💡265. Adding More State: Customer
    - step 1 - created one more initial state - created one more action creator (createCustomer)
    - step 2 - created its second action creator
    - step 3 - rename first reducer to accountReducer - create second reducer (customerReducer)
    - step 4 - combine reducers via combineReducers() - show in console the interim output
    - step 5 - dispatch actions from the new second Reducer to show how it works
    
    - in this lecture: create some more action creators and a reducer about the customer state
    - next: organize this code into a more organized structure
    
    Desired behavior:
    - Create new Reducer and combine our 2 Reducers into a Store
    
    Underlying implementation:
    - Create new initial state, create 2 new action creators, create second reducer (customerReducer, 2 cases)
    - Create rootReducer (using combineReducers), pass it to createStore()
    - Dispatch actions from this new combined Store and show that it works

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -38,7 +44,30 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: "account/deposit", payload: 500 });
 // console.log(store.getState());
@@ -84,4 +113,19 @@ store.dispatch(requestLoan(1000, "Buy a car"));
 console.log(store.getState());
 
 store.dispatch(payLoan());
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("Olzhas Dossaibek", "111"));
+store.dispatch(deposit(250));
 console.log(store.getState());
