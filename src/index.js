333d5dbc3961bde96681e6866130fff5afa016ea
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-01-16 19:35:40 +0500

    🛠️💡267. Back to React! Connecting our Redux App With React
    - step 1 - installs React Redux via terminal - imports it to index.js and uses (Provider, store Prop)
    - step 2 - use the Redux in Customer.js via useSelector
    
    - in this lecture: connect our Redux Store with the React App
    - next: how to dispatch actions to the Store
    
    Desired behavior:
    - Connect our React App with Redux
    
    Underlying implementation:
    - Install npm package, import and use Provider + Props
    - Customer.js: access State via useSelector, use the State info

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-
 import store from "./store";
+import { createCustomer } from "./features/customers/customerSlice";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
+// temp line for test
+store.dispatch(createCustomer("OlzhasX", 111));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
